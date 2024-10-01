9aeaaa120d266340d1baf6110d4229b650297f24
shivprasad150204 <shivneshshivneel.prasad@my.jcu.edu.au>
2024-10-01 16:01:44 +1000

    added convert_temps

---
+++ prac_02/convert_temps.py
@@ -0,0 +1,30 @@
+"""
+CP1404/CP5632 - Practical - Suggested Solution
+Convert temperatures between files
+"""
+
+import random
+
+def main():
+    """Convert temperatures from input file to output file."""
+    with open("temps_input.txt", "r") as input_file, open("temps_output.txt", "w") as output_file:
+        for line in input_file:
+            result = convert_fahrenheit_to_celsius(float(line))
+            print(result, file=output_file)
+
+def create_input_file(quantity):
+    """Write a specified number of random temperatures to a file."""
+    with open("temps_input.txt", "w") as temperatures_file:
+        for _ in range(quantity):
+            temperature = random.uniform(-200, 200)
+            print(temperature, file=temperatures_file)
+
+def convert_celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return celsius * 9.0 / 5 + 32
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * 5 / 9
+
+main()
