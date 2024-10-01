e1811141fa35add1aecc6b7a79a01a90c200b301
shivprasad150204 <shivneshshivneel.prasad@my.jcu.edu.au>
2024-10-01 15:35:23 +1000

    Add prac 1 files

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -5,8 +5,7 @@ Simplified program to determine score status
 
 score = float(input("Enter score: "))
 
-if not 0 <= score <= 100:
-    print("Invalid score")
+if not 0 <= score <= 100:    print("Invalid score")
 elif score >= 90:
     print("Excellent")
 elif score >= 50:
